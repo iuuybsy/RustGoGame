63f1d7916aaae3fb3b5b116a6a7fd01e80d130ca
mfdlshz <mfdlshz@163.com>
2025-06-16 23:18:33 +0800

    Fix bug in counting liberty logic and implement set stone logic.

--- src/logic/go_logic.rs
+++ src/logic/go_logic.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque};
 
 const LOGIC_WIDTH: usize = 19;
 const LOGIC_HEIGHT: usize = 19;
@@ -17,6 +17,7 @@ enum Occupy {
 pub struct GoLogic {
     is_black_turn: bool,
     board: [[Occupy; LOGIC_HEIGHT]; LOGIC_WIDTH],
+    related_stones: VecDeque<(usize, usize)>,
 }
 
 impl GoLogic {
@@ -24,6 +25,7 @@ impl GoLogic {
         GoLogic{
             is_black_turn: true, 
             board: [[Occupy::Free; LOGIC_HEIGHT]; LOGIC_WIDTH],
+            related_stones: VecDeque::new(),
         }
     }
 
@@ -45,7 +47,7 @@ impl GoLogic {
         self.board[x][y] != Occupy::Free
     }
 
-    pub fn get_local_liberty(&self, x:usize, y: usize) -> i32 {
+    pub fn get_local_liberty(&mut self, x:usize, y: usize, need_record: bool) -> i32 {
         let mut local_liberty: i32 = 0;
 
         if !self.is_cord_valid(x, y) {
@@ -68,41 +70,120 @@ impl GoLogic {
         while !stack.is_empty() {
             
             if let Some((x_cri, y_cri)) = stack.pop_back() {
+                if need_record {
+                    self.related_stones.push_back((x_cri, y_cri));
+                }
+
                 for i in 0..SEARCH_DIRECTION.len() {
-                    let x_next_add = x_cri + SEARCH_DIRECTION[i][0];
-                    let y_next_add = y_cri + SEARCH_DIRECTION[i][1];
-                    if !self.is_cord_valid(x_next_add, y_next_add) {
+                    let x_next = x_cri + SEARCH_DIRECTION[i][0];
+                    let y_next = y_cri + SEARCH_DIRECTION[i][1];
+                    if !self.is_cord_valid(x_next, y_next) {
                         continue;
                     }
-                    else if !unvisited[x_next_add][y_next_add] {
+                    else if !unvisited[x_next][y_next] {
                         continue;
                     }
 
-                    if self.board[x_next_add][y_next_add] == friend {
-                        stack.push_back((x_next_add, y_next_add));
+                    if self.board[x_next][y_next] == friend {
+                        stack.push_back((x_next, y_next));
                     }
-                    else if self.board[x_next_add][y_next_add] == Occupy::Free {
+                    else if self.board[x_next][y_next] == Occupy::Free {
                         local_liberty += 1;
                     }
-                    unvisited[x_next_add][y_next_add] = false;
-                    
+                    unvisited[x_next][y_next] = false;
+                }
+                for i in 0..SEARCH_DIRECTION.len() {
                     if x_cri >= SEARCH_DIRECTION[i][0] && y_cri >= SEARCH_DIRECTION[i][1] {
-                        let x_next_sub = x_cri - SEARCH_DIRECTION[i][0];
-                        let y_next_sub = y_cri - SEARCH_DIRECTION[i][1];
-                        if self.board[x_next_sub][y_next_sub] == friend {
-                            stack.push_back((x_next_sub, y_next_sub));
+                        let x_next = x_cri - SEARCH_DIRECTION[i][0];
+                        let y_next = y_cri - SEARCH_DIRECTION[i][1];
+                        if !self.is_cord_valid(x_next, y_next) {
+                            continue;
                         }
-                        else if self.board[x_next_sub][y_next_sub] == Occupy::Free {
+                        else if !unvisited[x_next][y_next] {
+                            continue;
+                        }
+
+                        if self.board[x_next][y_next] == friend {
+                            stack.push_back((x_next, y_next));
+                        }
+                        else if self.board[x_next][y_next] == Occupy::Free {
                             local_liberty += 1;
                         }
-                        unvisited[x_next_sub][y_next_sub] = false;
+                        unvisited[x_next][y_next] = false;
                     }
-
                 }
             }
-
         }
         return local_liberty;
+    }
+
+    pub fn set_stone(&mut self, x: usize, y: usize) {
+        if !self.is_cord_valid(x, y) {
+            println!("Cord ({}, {}) is not valid.", x, y);
+            return;
+        }
+        else if self.is_occupied_by_stone(x, y) {
+            println!("Cord ({}, {}) is not occupied by stone.", x, y);
+            return;
+        }
+
+        let friend = if self.is_black_turn {Occupy::Black} else {Occupy::White};
+        let hostile = if self.is_black_turn {Occupy::White} else {Occupy::Black};
+        self.board[x][y] = friend;
+
+        let local_liberty = self.get_local_liberty(x, y, false);
+
+        if local_liberty == 0 {
+            self.board[x][y] = Occupy::Free;
+            return;
+        }
+        for i in 0..SEARCH_DIRECTION.len() {
+            let x_next = x + SEARCH_DIRECTION[i][0];
+            let y_next = y + SEARCH_DIRECTION[i][1];
+
+            if !self.is_cord_valid(x_next, y_next) {
+                continue;
+            }
+            else if self.board[x_next][y_next] != hostile {
+                continue;
+            }
+            let hostile_liberty = self.get_local_liberty(x_next, y_next, true);
+            if hostile_liberty == 0 {
+                while !self.related_stones.is_empty() {
+                    if let Some((x_cur, y_cur)) = self.related_stones.pop_back() {
+                        self.board[x_cur][y_cur] = Occupy::Free;
+                    }
+                }
+            }
+            else {
+                self.related_stones.clear();
+            }
+        }
+        for i in 0..SEARCH_DIRECTION.len() {
+            if x >= SEARCH_DIRECTION[i][0] && y >= SEARCH_DIRECTION[i][1] {
+                let x_next = x - SEARCH_DIRECTION[i][0];
+                let y_next = y - SEARCH_DIRECTION[i][1];
+
+                if !self.is_cord_valid(x_next, y_next) {
+                    continue;
+                }
+                else if self.board[x_next][y_next] != hostile {
+                    continue;
+                }
+                let hostile_liberty = self.get_local_liberty(x_next, y_next, true);
+                if hostile_liberty == 0 {
+                    while !self.related_stones.is_empty() {
+                        if let Some((x_cur, y_cur)) = self.related_stones.pop_back() {
+                            self.board[x_cur][y_cur] = Occupy::Free;
+                        }
+                    }
+                }
+                else {
+                    self.related_stones.clear();
+                }
+            }
+        }
+        self.is_black_turn = !self.is_black_turn;
     }
 
     pub fn print_board_info(&self) {
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,19 @@ mod logic;
 fn main() {
     println!("Hello, world!");
     let mut logic = logic::go_logic::GoLogic::new();
-    logic.print_board_info();
-    logic.roughly_set_black_stone(0, 0);
-    logic.print_board_info();
-    logic.roughly_set_white_stone(1, 0);
+    
+    logic.set_stone(0, 0);
     logic.print_board_info();
 
-    let res1 = logic.get_local_liberty(0, 0);
-    println!("local liverty at (0, 0) is {}.", res1);
+    logic.set_stone(1, 0);
+    logic.print_board_info();
 
-    let res2 = logic.get_local_liberty(1, 0);
-    println!("local liverty at (1, 0) is {}.", res2);
+    logic.set_stone(1, 1);
+    logic.print_board_info();
+
+    logic.set_stone(0, 1);
+    logic.print_board_info();
+
+    logic.set_stone(0, 0);
+    logic.print_board_info();
 }
